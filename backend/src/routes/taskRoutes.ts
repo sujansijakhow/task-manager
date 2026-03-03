f6a3c1a7ad22157c912522cfe2266c923a57bdf5
sujansijakhow <sujansijakhow@gmail.com>
2026-03-03 15:50:00 +0545

    TASK CRUD Routes created

---
+++ backend/src/routes/taskRoutes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} from "../controllers/taskController";
+import { protect } from "../auth/auth.middleware"; 
+
+const router = express.Router();
+
+// All routes protected by JWT
+router.post("/", protect, createTask);
+router.get("/", protect, getTasks);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
+
+export default router;
