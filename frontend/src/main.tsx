3125bf3cbd51663e5f5407ed66f181d7e2346e52
sujansijakhow <sujansijakhow@gmail.com>
2026-03-05 21:33:47 +0545

    Redux Persist configured

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { store, persistor } from "./app/store";
 import App from "./App";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
