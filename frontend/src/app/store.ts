97c681e1d8cce4221e098be69da28c9f097eda64
sujansijakhow <sujansijakhow@gmail.com>
2026-03-05 21:31:41 +0545

    Task persitance configuration

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage"; 
+import { persistReducer, persistStore } from "redux-persist";
+
 import authReducer from "../features/auth/authSlice";
-import taskReducer from "../features/task/taskSlice"
+import taskReducer from "../features/task/taskSlice";
+
+// Persist config for tasks
+const taskPersistConfig = {
+  key: "tasks",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  tasks: persistReducer(taskPersistConfig, taskReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    tasks: taskReducer,
-  },
+  reducer: rootReducer,
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
